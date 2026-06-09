8d75ee850dc1eeb321f1667c3da1602c0371de1a
Alessandro Stefani <sandostef@gmail.com>
2026-06-09 11:10:34 +0200

    fix: swagger yaml now indented

--- server/src/routes/admin.routes.ts
+++ server/src/routes/admin.routes.ts
@@ -129,52 +129,52 @@ router.patch('/users/:id/role', requireAuth, requireAdmin, changeUserRole);
 /**
  * @openapi
  * /api/admin/documents/{id}/logs:
- * get:
- * summary: Ottieni lo storico completo dei log di un documento (Solo Admin)
- * tags:
- * - Admin
- * security:
- * - BearerAuth: []
- * parameters:
- * - in: path
- * name: id
- * required: true
- * schema:
- * type: string
- * description: ID del documento
- * responses:
- * 200:
- * description: Array di audit logs
- * 401:
- * description: Non autorizzato
- * 403:
- * description: Privilegi di amministratore richiesti
+ *  get:
+ *      summary: Ottieni lo storico completo dei log di un documento (Solo Admin)
+ *      tags:
+ *          - Admin
+ *      security:
+ *          - BearerAuth: []
+ *      parameters:
+ *          - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *              type: string
+ *          description: ID del documento
+ *      responses:
+ *          200:
+ *              description: Array di audit logs
+ *          401:
+ *              description: Non autorizzato
+ *          403:
+ *              description: Privilegi di amministratore richiesti
  */
 router.get('/documents/:id/logs', requireAuth, requireAdmin, getAdminDocumentLogs);
 
 /**
  * @openapi
  * /api/admin/documents/{id}:
- * get:
- * summary: Ottieni le informazioni base di un documento (Solo Admin)
- * tags:
- * - Admin
- * security:
- * - BearerAuth: []
- * parameters:
- * - in: path
- * name: id
- * required: true
- * schema:
- * type: string
- * description: ID del documento
- * responses:
- * 200:
- * description: Informazioni recuperate
- * 401:
- * description: Non autorizzato
- * 403:
- * description: Privilegi di amministratore richiesti
+ *  get:
+ *      summary: Ottieni le informazioni base di un documento (Solo Admin)
+ *      tags:
+ *          - Admin
+ *      security:
+*           - BearerAuth: []
+ *      parameters:
+ *          - in: path
+ *          name: id
+ *          required: true
+ *          schema:
+ *              type: string
+ *              description: ID del documento
+ *      responses:
+ *          200:
+ *              description: Informazioni recuperate
+ *          401:
+ *              description: Non autorizzato
+ *          403:
+ *              description: Privilegi di amministratore richiesti
  */
 router.get('/documents/:id', requireAuth, requireAdmin, getAdminDocumentInfo);
 
